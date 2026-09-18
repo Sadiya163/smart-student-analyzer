e6b9dc0af96dcbbb94275d66197ea11f2240078f
Sadiya163 <sadiyagurramkonda16@gmail.com>
2026-09-18 11:52:08 +0530

    Fix database connection for Aiven

--- app.py
+++ app.py
@@ -16,11 +16,12 @@ app.secret_key = os.environ.get("SECRET_KEY", "change-this-secret-key")
 def get_db():
     return mysql.connector.connect(
         host=os.environ.get("DB_HOST", "localhost"),
+        port=int(os.environ.get("DB_PORT", "3306")),
         user=os.environ.get("DB_USER", "root"),
         password=os.environ.get("DB_PASSWORD", ""),
-        database=os.environ.get("DB_NAME", "student_analyzer")
+        database=os.environ.get("DB_NAME", "student_analyzer"),
+        ssl_verify_cert=False
     )
-
 
 def calculate_average(marks):
     return round(sum(marks.values()) / len(marks), 2) if marks else 0
